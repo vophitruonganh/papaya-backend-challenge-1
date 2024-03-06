334d16f417d2cc875ce8f6e7c95213dc567b2515
vophitruonganh <truonganh.vo@popsww.com>
2024-03-06 09:26:08 +0700

    Implement logic get and resize image from S3

--- index.ts
+++ index.ts
@@ -9,12 +9,26 @@ const bucketName: string = process.env.BUCKET_NAME as string;
 const getImage = async (event: EventDto): Promise<any> => {
 	try {
 		// parse params
+		const imageKey: string | undefined = event?.pathParameters?.imageName;
+		if (!imageKey) return {statusCode: 400, body: 'Image name is required'};
+
+		const [_, imageExtension] = imageKey.split('.');
+
+		if (imageExtension !== 'jpg' && imageExtension !== 'jpeg' && imageExtension !== 'png')
+			return {statusCode: 400, body: 'Image extension not supported'};
+
+		const queryStringParameters: { width: string, height: string, v: string } = event?.queryStringParameters;
+		const width: number = parseInt(queryStringParameters?.width);
+		const height: number = parseInt(queryStringParameters?.height);
 
 		// get images from S3
+		let imageBuffer: Buffer = await getFileS3(bucketName, imageKey);
 
 		// Validate image exists
+		if (!imageBuffer) return {statusCode: 404, body: 'Image not found'};
 
 		// Resize image
+		if (width) imageBuffer = await resizeImage(imageBuffer, width, height);
 
 		return {
 		};
@@ -26,5 +40,20 @@ const getImage = async (event: EventDto): Promise<any> => {
 		};
 	}
 };
+
+const getFileS3 = async (bucketName: string, key: string): Promise<Buffer> => {
+	const data: any = await s3.getObject({Bucket: bucketName, Key: key}).promise();
+	return data?.Body as Buffer;
+}
+
+const resizeImage = async (imageBuffer: Buffer, width: number, height: number): Promise<Buffer> => {
+	let resizeOptions: sharp.ResizeOptions = {width: width};
+
+	if (height) resizeOptions.height = height;
+	else resizeOptions.fit = sharp.fit.contain;
+	return sharp(imageBuffer)
+		.resize(resizeOptions)
+		.toBuffer();
+}
 
 exports.handler = getImage
